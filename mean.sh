088417d21fd584950a003b0a8aa94b8ddec908f8
jxyang@wisc.edu <jxyang@wisc.edu>
2022-03-08 12:58:57 -0600

    Cleaned up code and added source

--- mean.sh
+++ mean.sh
@@ -6,11 +6,6 @@ if [[ !(($# -eq 1) || ($# -eq 2)) ]]; then
     exit 0
 fi
 
-file=${2:-/dev/stdin}
-#if [[ $# -eq 3]]; then
-#    file=$2
-#else
-#    file=/dev/stdin
-#fi
+file=${2:-/dev/stdin} # Derived from https://stackoverflow.com/questions/6980090/how-to-read-from-a-file-or-standard-input-in-bash
 
 cat $file | cut -d, -f $1 | tail -n +2 | { sum=0; counts=0; while read n; do sum=$(($sum+$n)); counts=$(($counts+1)); done; mean=$(echo "scale = 5; $sum / $counts" | bc); echo $mean;}
