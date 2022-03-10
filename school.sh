3ebbf5c174673326e45bb0cbb347d00ec17f13ad
Supraja <battula3@wisc.edu>
2022-03-09 23:38:18 -0600

    found average

--- school.sh
+++ school.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+head Property_Tax_Roll.csv | grep "MADISON SCHOOLS" |  cut -d, -f 7 |  { sum=0 count=0; while read n; do sum=$(($sum + $n)) count=$(($count+1)); done; avg=$(echo "scale=6; $sum/$count" | bc); echo $avg; }
+
