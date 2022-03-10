2528101e3baad3ec112251a901969dba8042fd7c
Grace Campbell <gacampbell@wisc.edu>
2022-03-10 08:00:35 -0600

    adding digits

--- digits.sh
+++ digits.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+pattern="([0-1]){4}"
+nums=0
+
+for i in $(seq 1000 2000)
+do
+    if [[ $i =~ $pattern ]]
+    then
+       nums=$(($nums + $i))
+    else
+	continue
+    fi
+    
+done
+
+echo $nums
+	   
+	  
+    
